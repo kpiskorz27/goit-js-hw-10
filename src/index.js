56821efa89eb576dbfb2d78d2e83b7f0f6161d82
kpiskorz27 <kpiskorz@gmx.at>
2024-02-23 18:42:29 +0100

    HW 10 - 1

--- src/index.js
+++ src/index.js
@@ -10,3 +10,30 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+function renderSelect(breeds) {
+  const markup = breeds
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>`;
+    })
+    .join('');
+  breedSelect.insertAdjacentHTML('beforeend', markup);
+  loader.classList.add('hidden');
+}
+breedSelect.addEventListener('change', e => {
+  loader.classList.remove('hidden');
+  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
+});
+
+function renderCat(catData) {
+  const { url } = catData;
+  const { description, name, temperament } = catData.breeds[0];
+  catInfo.insertAdjacentHTML(
+    'beforeend',
+    `<div><h2>${name}</h2>  <img src="${url}" alt="${name}" />
+        <p>${description}</p>
+        <p><strong>Temperament:</strong> ${temperament}</p>
+    </div>`
+  );
+  loader.classList.add('hidden');
+}
